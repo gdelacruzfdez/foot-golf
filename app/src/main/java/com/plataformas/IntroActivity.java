5bb82b11f599990f61dbe31853f7876d060fe616
dgsama <dgsama141@gmail.com>
2017-12-06 21:10:05 +0100

    prueba splash screen

---
+++ app/src/main/java/com/plataformas/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.plataformas;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
