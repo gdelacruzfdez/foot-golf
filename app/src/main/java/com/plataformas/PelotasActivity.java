f8ad6c424d72184e9289c887be2f5b027fcfcb44
dgsama <dgsama141@gmail.com>
2017-12-15 15:27:47 +0100

    Cambio de botones okei

--- app/src/main/java/com/plataformas/PelotasActivity.java
+++ app/src/main/java/com/plataformas/PelotasActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,16 +12,16 @@ import android.widget.Button;
 public class PelotasActivity extends Activity {
 
 
-    Button fut;
-    Button bask;
+    Button futbol;
+    Button basket;
     Button tenis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pelotas);
-        fut = (Button) findViewById(R.id.bFut);
-        bask = (Button) findViewById(R.id.bBask);
+        futbol = (Button) findViewById(R.id.bFut);
+        basket = (Button) findViewById(R.id.bBask);
         tenis = (Button) findViewById(R.id.bTenis);
 
 
@@ -38,28 +38,26 @@ public class PelotasActivity extends Activity {
         Button b = (Button) findViewById(view.getId());
         String cond = b.getText().toString();
         asignarActual(cond);
-/*
-        switch (cond) {
 
+        switch (cond) {
             case "f":
-                fut.setBackground(R.drawable.futa);
-                bask.setBackground(R.drawable.bascket);
-                tenis.setBackground(R.drawable.tenis);
+                futbol.setBackgroundResource( R.drawable.futa);
+                basket.setBackgroundResource(R.drawable.bascket);
+                tenis.setBackgroundResource(R.drawable.tenis);
                 break;
             case "t":
-
-                fut.setBackground(R.drawable.fut);
-                bask.setBackground(R.drawable.bascketa);
-                tenis.setBackground(R.drawable.tenis);
+                futbol.setBackgroundResource(R.drawable.fut);
+                basket.setBackgroundResource(R.drawable.bascket);
+                tenis.setBackgroundResource(R.drawable.tenisa);
                 break;
             case "b":
-
-                fut.setBackground(R.drawable.fut);
-                bask.setBackground(R.drawable.bascket);
-                tenis.setBackground(R.drawable.tenisa);
+                futbol.setBackgroundResource(R.drawable.fut);
+                basket.setBackgroundResource(R.drawable.bascketa);
+                tenis.setBackgroundResource(R.drawable.tenis);
                 break;
 
-*/
+
+        }
     }
 
     private void asignarActual(String cond) {
